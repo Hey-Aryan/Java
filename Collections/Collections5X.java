95277123da8fdfa7869b6c80fa06fdb14b9c594a
Hey-Aryan <142288080+Hey-Aryan@users.noreply.github.com>
2023-09-11 23:33:07 +0530

    Add files via upload

---
+++ Collections/Collections5X.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Collections5
+{
+    public static void main(String arg[])
+    {
+        ArrayList <Float>aobj = new ArrayList<Float>();
+
+        aobj.add(10.60);
+        
+    }
+}
+
+//this is array which internally works as linkedlist
+//dynamic arrary 
+//disadvantage of array is we cannot add between 
+
+//1 both classes are in java.util
+//2 both can provide functionalities manupulation and storage
+//3 array provides fast travesal 
+//4 kad ghal jasti asel tar use linkedlist 
+//5 direct acces karayche asel tar use array list 
+//eg : element on 780 
+//in array list direct it goes to 780
+// while linked list travelsal till 780
+
+
+// array list cant add elements in first 
